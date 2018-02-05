82c6c4b12c4cd3c6965668850e67b8a83ede0b7e
Macmini3 <systems@cogzidel.com>
2018-02-05 12:56:08 +0530

    source clean

--- babybuddy/wsgi.py
+++
@@ -1,9 +0,0 @@
-from django.core.wsgi import get_wsgi_application
-
-from dotenv import load_dotenv, find_dotenv
-
-# Environment variables
-# Check for and load environment variables from a .env file.
-load_dotenv(find_dotenv())
-
-application = get_wsgi_application()
